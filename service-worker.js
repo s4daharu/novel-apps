5ef9f0b736f9188b1e0ea21fa7d427246a4b939f
s4daharu <s4daharu@gmail.com>
2025-10-01 15:45:44 +0530

    v1.2.8

--- service-worker.js
+++ service-worker.js
@@ -17,10 +17,8 @@ const STATIC_PATHS = new Set([
 const CRITICAL_ICONS = [
   './icons/icon-72x72.png',
   './icons/icon-96x96.png',
-  './icons/icon-128x128.png',
-  './icons/icon-144x144.png',
-  './icons/icon-152x152.png',
-  './icons/icon-192x192.png'
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png'
 ];
 
 
